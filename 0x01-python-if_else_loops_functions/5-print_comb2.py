658186e45b9b4353a3dbaf53826747cd2fae41cc
frankMnjoroge <frankmugo@gmail.com>
2023-10-31 15:02:59 -0700

    program that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for char in range(0, 99):
     print("{:0d}".format(char), end=", ")
-    print("{:0d}".format(99))
+    print(f"{char}", end=", ")
+   # print("{:0d}".format(99))
