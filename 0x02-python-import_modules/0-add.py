b11bcb48685dc8b70b60ed5ac05c1156b5b26c59
frankMnjoroge <frankmugo@gmail.com>
2023-11-02 12:19:17 -0700

    0. Import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Import the add function from the add_0 module
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
