f45af14705efed96bc4df6c775cf32ec4c68e9d3
frankMnjoroge <frankmugo@gmail.com>
2023-11-02 17:46:18 -0700

    8. ByteCode -> Python #3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(y, z):
+    from magic_calculation_102 import add, sub
+    if y < z:
+        a = add(y, z)
+        for j in range(4, 6):
+            c = add(c, j)
+        return c
+    else:
+        return (sub(y, z))
