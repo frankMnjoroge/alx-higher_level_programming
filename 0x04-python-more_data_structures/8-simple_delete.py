a8020c442e8feac33bfa474d12c6d31595a93bf4
frankMnjoroge <frankmugo@gmail.com>
2023-11-08 12:18:10 -0800

    8. Simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,4 @@
+#!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    # Check if the key is present in the dictionary
-    if key in a_dictionary:
-        # If the key exists, delete it from the dictionary
-        del a_dictionary[key]
-        return a_dictionary
+    a_dictionary.pop(key, None)
+    return a_dictionary
