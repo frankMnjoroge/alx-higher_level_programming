9139615e69c0f0b0575e1d3b3348e13c8eb41b78
frankMnjoroge <frankmugo@gmail.com>
2023-12-05 08:20:34 -0800

    updated

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,12 +7,12 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int has;
+	unsigned long int hash;
 	int j;
 
-	has = 5381;
+	hash = 5381;
 	while ((j = *str++))
-		has = ((has << 5) + has) + j; /* has * 33 + j */
+		hash = ((has << 5) + has) + j; /* has * 33 + j */
 
-	return (has);
+	return (hash);
 }
