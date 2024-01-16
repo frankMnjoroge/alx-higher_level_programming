451c4c5e20f8a75c1888971dd8b3b5354b1ee0f0
frankMnjoroge <frankmugo@gmail.com>
2024-01-16 11:59:16 -0800

    updated

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---  inserts a new row in the first_table (database hbtn_0c_0) in  MySQL server
-INSERT INTO `first_table` (`id`, `name`) VALUES (id = 89, name = "Best School");
+--  inserts a new row in the first_table (database hbtn_0c_0)
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
