0fa9f83f7c962cbd4634d65c09af4556728b0514
frankMnjoroge <frankmugo@gmail.com>
2024-01-17 11:59:01 -0800

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+--  uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+-- tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- sorted in ascending order by the genre name
+SELECT name FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script that creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use database
+USE hbtn_0d_usa;
+-- creates table states with attributes id INT unique, auto generated, can’t be null
+-- and is a primary key name VARCHAR(256) can’t be null
+CREATE TABLE IF NOT EXISTS `state` (id INT UNIQUE NOT NULL AUTO_INCREAMENT,name VARCHAR(256) NOT NULL, PRIMARY KEY(id)); 
