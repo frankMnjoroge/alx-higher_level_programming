1b016f29d0006b9772fdf1205bd90a813e9590f7
frankMnjoroge <frankmugo@gmail.com>
2024-01-17 12:14:52 -0800

    updated

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
---  creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use datatabase
 USE hbtn_0d_usa;
 -- table city with attributes id INT unique, auto generated, can’t be null and is a primary key
 -- state_id INT, can’t be null and must be a FOREIGN KEY that references 
 CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	state_id INT NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY(state_id) REFERENCES states(id)); 
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id)); 
