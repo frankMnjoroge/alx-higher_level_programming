c344f94d4ed6fa32dc0dbacf6072d0fa073fc586
frankMnjoroge <frankmugo@gmail.com>
2024-02-29 07:24:51 -0800

    0x10. Python - Network #0

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Algorithms for list of integer"""
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
