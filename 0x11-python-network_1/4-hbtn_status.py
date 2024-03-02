67ba989159c589603a6c7d9aac1466292dab3766
frankMnjoroge <frankmugo@gmail.com>
2024-03-02 02:00:10 -0800

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ import requests
 if __name__ == "__main__":
     req = requests.get('https://alx-intranet.hbtn.io/status')
     txt = req.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txt), txt))
+    print('Body response:\n\t-type:{}\n\t- content:{}'.format(type(txt),txt))
